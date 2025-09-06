046141548c5d1fef7144ff06c697aa590fede7c8
rohitsharmaj7 <rohitsharmaj7@gmail.com>
2025-09-06 09:56:56 +0530

    feat/added-the-navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
+import Navbar from "~/components/Navbar";
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Apply Smart AI" },
+    { name: "description", content: "Apply smart and get hired quickly!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+  <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track your Application & Resume Rating</h1>
+        <h2>Review you CV with AI powered feedback</h2>
+      </div>
+    </section>
+  </main>);
 }
